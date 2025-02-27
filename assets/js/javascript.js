c5e47a913e319547f892a1eae198fee069109b0e
ramkumar <akramkumarbtech@gmail.com>
2025-02-27 14:15:36 +0530

    adding the submit response

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(".form");
 const inputField=document.querySelector(".email");
 const errorMsg=document.querySelector(".error-msg");
+const signup=document.querySelector(".sign-up-page");
+const submit= document.querySelector(".submit-page");
 const handsubmit = e => {
     e.preventDefault(e);
     const formData = new FormData(e.target);
